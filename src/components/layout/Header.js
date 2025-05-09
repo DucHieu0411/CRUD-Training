18f62457dc50eacf69bbc281da96d15c827dce67
Huy Duc Nguyen <huy.nguyenduc@asnet.com.vn>
2025-05-09 11:20:59 +0700

    Setup environments

---
+++ src/components/layout/Header.js
@@ -0,0 +1,11 @@
+import { Container, Nav, Navbar, NavDropdown, NavLink } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+    </div>
+  );
+};
+
+export default Header;
