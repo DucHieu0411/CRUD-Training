6a66d9c8c9ab3c50ee163999c142deb28b678eaa
Huy Duc Nguyen <huy.nguyenduc@asnet.com.vn>
2025-05-09 13:55:19 +0700

    Design Header UI

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,9 +2,31 @@ import { Container, Nav, Navbar, NavDropdown, NavLink } from "react-bootstrap";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+
+            <NavLink to="/users" className="nav-link">
+              Manage Users
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Setting">
+              <NavLink to="/login" className="nav-link">
+                Home
+              </NavLink>
+              <NavDropdown.Item>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
